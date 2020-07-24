4139ba84fc5a4c3599f18f5a3a2574568d602713
SARATH.S <68682390+sarathsdev@users.noreply.github.com>
2020-07-24 22:17:29 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,15 +1,15 @@
 <?php
 //define_syslog_variables
-$name = $_REQUEST['cf_name'];
-$email = $_REQUEST['cf_email'];
-$message = $_REQUEST['cf_message'];
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$message = $_REQUEST['message'];
 //check input fields
 if(empty($name) || empty($email) || empty($message))
 {
   echo "Please fill all the fields";
 }
 else{
-  mail("sarathsivan099@gmail.com","Personal Website message",$message,"From:$name <$email>" );
+  mail("pkd18cs048@gecskp.ac.in","Personal Website message",$message,"From:$name <$email>" );
 
   echo "<script type='text/javascript'>alert('Your message sent successfully');
   window.history.log(-1);
