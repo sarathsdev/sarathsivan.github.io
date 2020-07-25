09e33062c42fd94a44b090c01224a8149c57c611
SARATH.S <68682390+sarathsdev@users.noreply.github.com>
2020-07-25 14:30:14 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,14 +52,14 @@ const sr = ScrollReveal({
 /*SCROLL HOME*/
 sr.reveal('.home__title', {})
 sr.reveal('.home__scroll', {delay: 200})
-sr.reveal('.home__img', {origin:'right', delay: 400})
+sr.reveal('.home__img', {origin:'right', delay: 200})
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img', {delay: 500})
 sr.reveal('.about__subtitle', {delay: 300})
 sr.reveal('.about__profession', {delay: 400})
 sr.reveal('.about__text', {delay: 500})
-sr.reveal('.about__social-icon', {delay: 400})
+sr.reveal('.about__social-icon', {delay: 600 ,interval:200})
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle', {})
